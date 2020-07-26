bedb4e3eddb8790b4ac92b99451e8a50c8a8ae16
Mohammad Tareq <mdtareq320@gmail.com>
2020-07-26 15:34:18 +0600

    js updated

--- js/grade.js
+++ js/grade.js
@@ -4,26 +4,26 @@ const ictCalculate=document.getElementById("ict-calculate");
 const totalCalculate=document.getElementById("total-calculate");
 
 banglaCalculate.addEventListener('click',function(){
-    const banglaMark = document.getElementById('bangla-mark').value;
-    const banglaMarkNumber = parseFloat(banglaMark);
-    totalMark(banglaMarkNumber);
+    const banglaMarkNumber = subjectNumber('bangla-mark');
     let banglaGrade=numberToGrade(banglaMarkNumber);
     document.getElementById('bangla-grade').innerText=banglaGrade;
+    totalMark(banglaMarkNumber);
 });
+
 englishCalculate.addEventListener('click',function(){
-    const englishMark = document.getElementById('english-mark').value;
-    const englishMarkNumber = parseFloat(englishMark);
-    totalMark(englishMarkNumber);
+    const englishMarkNumber = subjectNumber('english-mark');
     let englishGrade=numberToGrade(englishMarkNumber);
     document.getElementById('english-grade').innerText=englishGrade;
+    totalMark(englishMarkNumber);
 });
+
 ictCalculate.addEventListener('click',function(){
-    const ictMark = document.getElementById('ict-mark').value;
-    const ictMarkNumber = parseFloat(ictMark);
-    totalMark(ictMarkNumber);
+    const ictMarkNumber = subjectNumber('ict-mark');
     let ictGrade=numberToGrade(ictMarkNumber);
     document.getElementById('ict-grade').innerText=ictGrade;
+    totalMark(ictMarkNumber);
 });
+
 totalCalculate.addEventListener('click',function(){
     const totalMark=parseFloat(document.getElementById("total").innerText);
     const averageMark=Math.ceil(totalMark/3);
@@ -33,9 +33,14 @@ totalCalculate.addEventListener('click',function(){
 
 function totalMark(subjectNumber){
     const addTotal=parseFloat(document.getElementById("total").innerText);
-    if(subjectNumber>=0&&subjectNumber<=100){
+    if(subjectNumber>=0 && subjectNumber<=100){
     const total=addTotal+subjectNumber;
     document.getElementById("total").innerText=total;}
+}
+
+function subjectNumber(subject){
+    const mark = document.getElementById(subject).value;
+    return parseFloat(mark);
 }
 
 function numberToGrade(mark){
@@ -47,7 +52,7 @@ function numberToGrade(mark){
             return "D";
         }
         else if(40<=mark&&mark<=49){
-            return "c";
+            return "C";
         }
         else if(50<=mark&&mark<=59){
             return "B";
@@ -64,6 +69,7 @@ function numberToGrade(mark){
     }
     return "F";
 }
+
 function overallGrade(averageNumber){
     const banglaGradeSign=document.getElementById('bangla-grade').innerText;
     const englishGradeSign=document.getElementById('english-grade').innerText;
